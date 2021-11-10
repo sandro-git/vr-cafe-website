a6c68ced2450e0282d2deefd19937024b36984e4
sandro <sandroazerty@icloud.com>
2021-11-10 16:14:03 +0100

    add links

--- components/MenuIcon.vue
+++ components/MenuIcon.vue
@@ -1,17 +1,35 @@
 <template>
   <ul class="flex">
     <li class="">
-      <img
-        src="~assets/f_logo_RGB-White_100.png"
-        alt="facebook logo"
-        class="h-8"
-      />
+      <a
+        href="https://www.facebook.com/vrcafe66/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img
+          src="~assets/f_logo_RGB-White_100.png"
+          alt="facebook logo"
+          class="h-8"
+        />
+      </a>
     </li>
     <li class="">
-      <img src="~assets/Instagram.png" alt="facebook logo" class="h-8" />
+      <a
+        href="https://www.instagram.com/vrcafe_66/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img src="~assets/Instagram.png" alt="facebook logo" class="h-8" />
+      </a>
     </li>
     <li class="">
-      <img src="~assets/Tripadvisor.png" alt="facebook logo" class="h-8" />
+      <a
+        href="https://www.tripadvisor.fr/Profile/420vrc?fid=e16823e7-88ff-4118-9930-11a77ec34d8e"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img src="~assets/Tripadvisor.png" alt="facebook logo" class="h-8" />
+      </a>
     </li>
   </ul>
 </template>
