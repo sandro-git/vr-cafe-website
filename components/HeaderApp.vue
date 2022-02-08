6d6b2490b57df309d16c2e1151c0715c8a71b59e
sandro <sandroazerty@icloud.com>
2022-02-08 23:49:08 +0100

    add simplybook me

--- components/HeaderApp.vue
+++ components/HeaderApp.vue
@@ -2,17 +2,7 @@
   <!-- This example requires Tailwind CSS v2.0+ -->
   <header class="bg-gray-900 lg:h-40 w-full z-20">
     <nav
-      class="
-        lg:max-h-40
-        w-full
-        py-6
-        grid grid-cols-2 grid-rows-2
-        lg:grid-rows-1 lg:grid lg:grid-cols-3
-        items-center
-        justify-between
-        lg:border-none
-        bg-gray-900
-      "
+      class="lg:max-h-40 w-full py-6 grid grid-cols-2 grid-rows-2 lg:grid-rows-1 lg:grid lg:grid-cols-3 items-center justify-between lg:border-none bg-gray-900"
       aria-label="Top"
     >
       <!-- logo -->
@@ -28,44 +18,21 @@
       <!-- bouton réserver -->
       <div class="flex justify-center lg:col-start-3">
         <a
-          href="reservation.html"
-          class="
-            inline-block
-            bg-white
-            py-2
-            px-4
-            border border-transparent
-            rounded-md
-            text-base
-            lg:text-2xl
-            font-medium
-            text-indigo-600
-            hover:bg-indigo-50
-          "
+          href="reservation-simply.html"
+          class="inline-block bg-white py-2 px-4 border border-transparent rounded-md text-base lg:text-2xl font-medium text-indigo-600 hover:bg-indigo-50"
           >Réserver</a
         >
       </div>
       <!-- navigation mobile -->
       <div
-        class="
-          col-span-2
-          lg:col-span-1 lg:col-start-2 lg:row-start-1
-          flex flex-col
-          items-center
-        "
+        class="col-span-2 lg:col-span-1 lg:col-start-2 lg:row-start-1 flex flex-col items-center"
       >
         <div class="mb-4 py-4 flex space-x-6">
           <!-- acceuil -->
           <nuxt-link
             key="Solutions"
             to="/"
-            class="
-              text-base
-              lg:text-2xl
-              font-medium
-              text-white
-              hover:text-indigo-50
-            "
+            class="text-base lg:text-2xl font-medium text-white hover:text-indigo-50"
           >
             Accueil
           </nuxt-link>
@@ -73,13 +40,7 @@
           <nuxt-link
             key="Pricing"
             to="#price"
-            class="
-              text-base
-              lg:text-2xl
-              font-medium
-              text-white
-              hover:text-indigo-50
-            "
+            class="text-base lg:text-2xl font-medium text-white hover:text-indigo-50"
           >
             Tarifs
           </nuxt-link>
@@ -87,13 +48,7 @@
           <nuxt-link
             key="Company"
             to="#escape"
-            class="
-              text-base
-              lg:text-2xl
-              font-medium
-              text-white
-              hover:text-indigo-50
-            "
+            class="text-base lg:text-2xl font-medium text-white hover:text-indigo-50"
           >
             Escape
           </nuxt-link>
@@ -101,13 +56,7 @@
           <nuxt-link
             key="Docs"
             to="#arcade"
-            class="
-              text-base
-              lg:text-2xl
-              font-medium
-              text-white
-              hover:text-indigo-50
-            "
+            class="text-base lg:text-2xl font-medium text-white hover:text-indigo-50"
           >
             Arcade
           </nuxt-link>
@@ -115,13 +64,7 @@
           <nuxt-link
             key="Docs"
             to="#culture"
-            class="
-              text-base
-              lg:text-2xl
-              font-medium
-              text-white
-              hover:text-indigo-50
-            "
+            class="text-base lg:text-2xl font-medium text-white hover:text-indigo-50"
           >
             Culture
           </nuxt-link>
